f73b910505d425ef267394015fad322fcc022a81
viniciusandrre <viniciusandre_pereira@outlook.com>
2024-06-18 10:07:14 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position pos = new Position(2, 6);
+		System.out.println(pos);
 
 	}
 
---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+	
+	public Position() {
+		super();
+	}
+
+	public Position(Integer row, Integer column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+	
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
