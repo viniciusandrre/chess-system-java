21c77d4a13e357825b36fd18ba61c4a07a4ccd78
viniciusandrre <viniciusandre_pereira@outlook.com>
2024-06-18 10:37:06 -0300

    Starting to implement board and piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,23 @@
 package boardgame;
 
 public class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+		super();
+	}
 
+	public Piece(Board board) {
+		super();
+		this.board = board;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
 }
