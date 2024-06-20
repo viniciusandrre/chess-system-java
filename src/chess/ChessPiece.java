004b466cddf11fb0828192752ac0fba258988d04
viniciusandrre <viniciusandre_pereira@outlook.com>
2024-06-20 11:07:48 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -15,6 +16,18 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}	
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMovement() {
+		moveCount++;
+	}
+	
+	public void decreaseMovement() {
+		moveCount--;
+	}
 	
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
